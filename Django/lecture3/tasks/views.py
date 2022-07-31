e75aa998bffb96d337614e97961736b0892e48ee
Muhammed Oladapo <muhammedoladapo3@gmail.com>
2022-07-31 21:51:31 +0100

    laze

--- Django/lecture3/tasks/views.py
+++ Django/lecture3/tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+tasks = ["bar", "foo", "far"]
+
 # Create your views here.
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
